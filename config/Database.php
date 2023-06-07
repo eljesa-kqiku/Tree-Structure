90fabd50648cd290d03c33416bb2bddf92d0a470
Eljesa Kqiku <eljesakqiku@gmail.com>
2023-06-07 23:59:06 +0200

    Connecting to database

---
+++ config/Database.php
@@ -0,0 +1,18 @@
+<?php
+$db_host = 'tree_db';
+$db_name = 'tree_structure';
+$db_username = 'tree';
+$port = "3306";
+$db_password = 'tree';
+
+    try{
+        $conn = new PDO('mysql:host='.$db_host.'; port='.$port.'; dbname='.$db_name,$db_username,$db_password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    }
+    catch(PDOException $e){
+        echo "Connection error ".$e->getMessage();
+        exit;
+    }
+
+
